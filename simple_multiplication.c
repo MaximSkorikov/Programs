0723546e884d342a1f9a718b61c8dd6f52dcae27
MaximSkorikov <skorikovmaks03@gmail.com>
2023-09-07 22:54:15 +0300

    Если число четное, умножить на 8, если нет, на 9

---
+++ simple_multiplication.c
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/583710ccaa6717322c000105
+int simple_multiplication(int number) 
+{
+    return number = (number % 2 == 0) ? (number << 3) : (number << 3) + number;
+}
